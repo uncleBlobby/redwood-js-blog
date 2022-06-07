d2955e21dccc8cbad2bf5e8c5c737612918606cc
Dustin <dc.christianson@gmail.com>
2022-06-07 17:25:24 -0600

    finished chapter 1

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>
+        This blog was created to document my dev journey and share my thoughts
+        on the web!
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
